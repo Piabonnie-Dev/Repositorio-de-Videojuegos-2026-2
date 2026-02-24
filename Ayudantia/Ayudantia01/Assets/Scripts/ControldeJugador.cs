28fe4a4b71af5eb6404d2f1ac8dbf483bb43b5e3
Piabonnie <elliot.villafana.pasten.408@gmail.com>
2026-02-23 18:45:48 -0600

    Practica02_3D terminado
    Este es el commit de la practica 02 terminado

---
+++ Ayudantia/Ayudantia01/Assets/Scripts/ControldeJugador.cs
@@ -0,0 +1,29 @@
+ using UnityEngine;
+
+public class ControldeJugador : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(h != 0)
+        {
+            Debug.Log("Movimiento horizontal: " + h);
+
+        }
+
+if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Salto");
+        }
+
+
+
+    }
+}
--- Laboratorio/Practica02_3D/Assets/Scripts/Camera/SmoothFollow.cs
+++ Laboratorio/Practica02_3D/Assets/Scripts/Camera/SmoothFollow.cs
@@ -16,7 +16,7 @@ public float smoothSpeed = 10f;
     // Update is called once per frame
     void LateUpdate()
     {
- Vector3 desiredPos = target.position - target.forward * distance
+        Vector3 desiredPos = target.position - target.forward * distance
 + Vector3.up * height;
 transform.position = Vector3.Lerp(transform.position,
 desiredPos, smoothSpeed * Time.deltaTime);
