42ecdf081d00648da46f75fc1ff41da82337edba
Piabonnie <elliot.villafana.pasten.408@gmail.com>
2026-03-16 19:22:30 -0600

    git de la practica 04 en 3D laboratorio

---
+++ Laboratorio/Practica04_3D/Assets/Scripts/Agent/ClickToMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            agent.SetDestination(hit.point);
+        }
+        }
+    }
+}
